bf7be4ad8992e94ea07557280036cf90bd808d9c
shellhue <zeyu_huang@163.com>
2018-09-04 15:28:14 +0800

    init cifar10

---
+++ resnet.py
@@ -0,0 +1,89 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        stride = 1
+        if in_channels != out_channels:
+            stride = 2
+        self.left = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        self.shortcut = nn.Sequential()
+        if in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+
+    def forward(self, x):
+        out = self.left(x)
+        out += self.shortcut(x)
+        out = F.relu(out)
+
+        return out
+
+
+class ResNet(nn.Module):
+    def __init__(self, num_blocks=3):
+        super(ResNet, self).__init__()
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(16),
+            nn.ReLU(),
+        )
+        self.layer1 = self.make_layer(16, 16,  num_blocks)
+        self.layer2 = self.make_layer(16, 32, num_blocks)
+        self.layer3 = self.make_layer(32, 64, num_blocks)
+        self.fc = nn.Linear(512, 10)
+
+    def make_layer(self, in_channels, out_channels, num_blocks):
+        layers = []
+        for i in range(num_blocks):
+            if i == 0:
+                layers.append(ResidualBlock(in_channels, out_channels))
+            else:
+                layers.append(ResidualBlock(out_channels, out_channels))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = F.avg_pool2d(x, 8)
+        x = x.view(x.size(0), -1)
+        out = self.fc(x)
+
+        return out
+
+
+def ResNet20():
+    return ResNet(num_blocks=3)
+
+
+def ResNet32():
+    return ResNet(num_blocks=5)
+
+
+def ResNet44():
+    return ResNet(num_blocks=7)
+
+
+def ResNet56():
+    return ResNet(num_blocks=9)
+
+
+def ResNet110():
+    return ResNet(num_blocks=18)
+
+
+def ResNet1202():
+    return ResNet(num_blocks=200)
+
---
+++ train.py
@@ -0,0 +1,98 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from resnet import ResNet20
+
+
+# super parameters
+EPOCH = 100
+BATCH_SIZE = 128
+LR = 0.1
+
+
+# use GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+# data preparing
+# data augmentation
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),  # random crop with 4 padding
+    transforms.RandomHorizontalFlip(),  # random horizontal flip
+    transforms.ToTensor(),  # convert to tensor
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),  # normalize rgb
+])
+
+transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
+
+training_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
+training_loader = torch.utils.data.DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=2)
+
+test_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=100, shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')  # labels
+
+
+# create resnet model
+resnet20 = ResNet20().to(device)
+
+criterion = nn.CrossEntropyLoss()  # use cross entropy loss
+# sgd optimizer with momentum and L2 regulation
+optimizer = optim.SGD(resnet20.parameters(), lr=LR, momentum=0.9, weight_decay=5e-4)
+
+
+# train
+if __name__ == "__main__":
+    print("Start Training!")
+    for epoch in range(0, EPOCH):
+        print('\nEpoch: %d' % (epoch + 1))
+        resnet20.train()
+        sum_loss = 0.0
+        correct = 0.0
+        total = 0.0
+        for i, data in enumerate(training_loader, 0):
+            length = len(training_loader)
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            optimizer.zero_grad()
+
+            # forward -> backward -> update gradients
+            outputs = resnet20(inputs)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+
+            # 每训练1个batch打印一次loss和准确率
+            sum_loss += loss.item()
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += predicted.eq(labels.data).cpu().sum()
+            print('[epoch:%d, iter:%d] Loss: %.03f | Acc: %.3f%% '
+                  % (epoch + 1, (i + 1 + epoch * length), sum_loss / (i + 1), 100. * correct / total))
+
+        # 每训练完一个epoch测试一下准确率
+        print("Waiting Test!")
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for data in test_loader:
+                resnet20.eval()
+                images, labels = data
+                images, labels = images.to(device), labels.to(device)
+                outputs = resnet20(images)
+                # 取得分最高的那个类 (outputs.data的索引号)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum()
+            print('测试分类准确率为：%.3f%%' % (100 * correct / total))
+            acc = 100. * correct / total
+            # 将每次测试结果实时写入acc.txt文件中
+            print('Saving model......')
+            torch.save(resnet20.state_dict(), '%s/net_%03d.pth' % ("./log", epoch + 1))
+    print("Training Finished, TotalEPOCH=%d" % EPOCH)
