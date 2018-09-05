a3630ba59a8e587dea9f5af66381dfad7a114906
shellhue <zeyu_huang@163.com>
2018-09-05 08:10:01 +0800

    remove layers

--- onnx2pb.py
+++ onnx2pb.py
@@ -21,10 +21,10 @@ onnx_model = onnx.load(ONNX_PROTO_FILE)
 init_net, predict_net = Caffe2Backend.onnx_graph_to_caffe2_net(onnx_model.graph)
 with open("onnx-init-20.pb", "wb") as f:
     f.write(init_net.SerializeToString())
-with open("onnx-init-20.pbtxt".format(LAYERS), "w") as f:
+with open("onnx-init-20.pbtxt", "w") as f:
     f.write(str(init_net))
-with open("onnx-predict-20.pb".format(LAYERS), "wb") as f:
+with open("onnx-predict-20.pb", "wb") as f:
     f.write(predict_net.SerializeToString())
-with open("onnx-predict-20.pbtxt".format(LAYERS), "w") as f:
+with open("onnx-predict-20.pbtxt", "w") as f:
     f.write(str(predict_net))
 
